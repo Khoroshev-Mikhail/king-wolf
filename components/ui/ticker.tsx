550789f8907150446e7d60c17a83c2069ca92d16
Mikha38 <oyks1379@yandex.ru>
2023-05-05 22:11:49 +0300

    meta

--- components/ui/ticker.tsx
+++ components/ui/ticker.tsx
@@ -9,11 +9,11 @@ export default function Ticker({ green } : { green?: boolean }){
         <div className={`${green ? 'rotate-[-2deg]' : 'rotate-[2deg]'} relative mx-[-20px] md:mx-[-40px] lg:mx-[-60px] xl:mx-[-80px] 2xl:mx-[-200px]`}>
             <Marquee autoFill direction={green ? 'left' : 'right'}>
                 <div className={`${drukCyrBold} ${green ? 'bg-my_green text-my_blue' : 'bg-my_blue'} mx-[-1px] uppercase flex h-[32px] text-[22px] sm:text-[30px] sm:h-[56px]`}>
-                    <div className="px-[10px] flex flex-col justify-center">
+                    <div className="px-[9px] flex flex-col justify-center">
                         <Image src={wolf} alt="KING WOLF" className="w-auto h-[26px] sm:h-[41px]"/>
                     </div>
                     <div className="flex flex-col justify-center">
-                        <p className="px-[10px]">King wolf</p>
+                        <p className="px-[9px]">King wolf</p>
                     </div>
                 </div>
             </Marquee>
