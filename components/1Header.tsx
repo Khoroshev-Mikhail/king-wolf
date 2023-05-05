8edb8c2bc784e20152a5f6d88f46a0d6e16d160a
Mikha38 <oyks1379@yandex.ru>
2023-05-05 20:24:41 +0300

    optimizet mobile

--- components/1Header.tsx
+++ components/1Header.tsx
@@ -15,6 +15,7 @@ export default function Header(){
         <div className="relative h-screen sm:h-auto min-h-[600px] mb-[70px]">
 
             {/* background-eclipse */}
+            <div className="absolute z-20 sm:hidden h-full min-h-screen bottom-0 right-0 w-full xs:max-w-[450px] bg-[url('/images/top-grid.png')] bg-no-repeat bg-bottom bg-contain"></div>
             <div className="absolute z-10 hidden sm:block h-full min-h-screen top-0 w-[75%] bg-[url('/images/eclipse-header-left.svg')] sm:bg-[url('/images/eclipse-header-left-big.svg')] bg-no-repeat bg-left-top bg-contain"></div>
             <div className="absolute z-10 sm:hidden h-full min-h-screen top-0 w-[75%] bg-[url('/images/eclipse-header-left-mobile.svg')] sm:bg-[url('/images/eclipse-header-left-big.svg')] bg-no-repeat bg-left-top bg-contain"></div>
             <div className="absolute z-10 sm:hidden h-full min-h-screen top-0 right-0 w-[50%] bg-[url('/images/eclipse-header-right.svg')] bg-no-repeat bg-right-bottom bg-contain"></div>
@@ -65,7 +66,7 @@ export default function Header(){
                     Shiba inu had an affair<br className="sm:hidden" /> with a wolf and a wolf cub was born. His mission is to unite everyone and become the king of memecoins
                 </p>
                 <div className="">
-                    <Image priority src={bigwolf_mobile} alt="King wolf" className="absolute z-0 left-0 bottom-[-70px] w-full sm:hidden" />
+                    <Image priority src={bigwolf_mobile} alt="King wolf" className="absolute z-0 right-0 top-[100px] w-full xs:max-w-[450px] sm:hidden" />
                     <Image src={bigwolf} alt="King wolf" className="absolute z-0 top-0 right-0 w-[50%] max-w-[700px] hidden sm:inline-flex" loading="lazy"/> 
                 </div>
                 <div className="absolute z-20 left-0 w-full px-[20px] bottom-[20px] sm:relative sm:px-0 sm:mt-[20px] sm:bottom-[0]">
