b1d7b859322895831a2950349f1a547da27faeae
Mikha38 <oyks1379@yandex.ru>
2023-05-07 11:33:18 +0300

    home

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ export default function Home() {
     <Head>
         <title>King Wolf</title>
         <meta httpEquiv="content-language" content="en" />
+        <meta name="description" content="Shiba inu had an affair with a wolf and a wolf cub was born. His mission is to unite everyone and become the king of memecoins" />
         <link rel="shortcut icon" href="/images/wolf-ticker.png" />
     </Head>
       <Header />
